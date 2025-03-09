7887f1221035ce51825013802771e769b680ebe5
sumitm102 <sumitmandal0102@gmail.com>
2025-03-09 11:05:26 -0400

    a

--- client/src/components/data-charts.tsx
+++ client/src/components/data-charts.tsx
@@ -225,7 +225,7 @@ export const DataCharts = () => {
             <TransactionsTable transactions={transactions} />
           </div>
         </div>
-        <div className="flex-1 flex items-stretch">
+        <div className="flex-1 flex items-stretch mt-10">
           <div className="w-full h-full">
             <MapComponent transactions={transactions} />
           </div>
